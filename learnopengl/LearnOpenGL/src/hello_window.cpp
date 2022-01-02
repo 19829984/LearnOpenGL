1b3cabe86a3ebaf0bd662d4c10974b1f28c0d38c
Bowen Wu <fsdi321@mit.edu>
2022-01-01 22:32:59 -0500

    Finish triangle with Element Buffer Object

--- learnopengl/LearnOpenGL/src/hello_window.cpp
+++ learnopengl/LearnOpenGL/src/hello_window.cpp
@@ -6,11 +6,28 @@
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+//float vertices[] = {
+//    // positions         // colors
+//     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+//    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+//     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+//};
+
 float vertices[] = {
-    // positions         // colors
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+};
+unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+};
+
+float texCoords[] = {
+    0.0f, 0.0f,  // lower-left corner  
+    1.0f, 0.0f,  // lower-right corner
+    0.5f, 1.0f   // top-center corner
 };
 
 int main() {
@@ -38,6 +55,7 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
@@ -47,19 +65,24 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
+
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     int  success;
     char infoLog[512];
-
-    Shader ourShader("C:\\Users\\Bowen\\Desktop\\OpenGL Tutorial\\learnopengl\\LearnOpenGL\\src\\shader_vertex.glsl", "C:\\Users\\Bowen\\Desktop\\OpenGL Tutorial\\learnopengl\\LearnOpenGL\\src\\shader_frag.glsl");
+    Shader ourShader(SOLUTIONDIR"\\src\\shader_vertex.glsl", SOLUTIONDIR"\\src\\shader_frag.glsl");
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -71,7 +94,8 @@ int main() {
         ourShader.setFloat("xOffset", 0.0f);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
